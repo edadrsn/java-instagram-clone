a7ce24bc53d39c20ea7d280b76fd988849293f11
Eda Dursun <edadrsn929@gmail.com>
2025-02-12 20:41:54 +0300

    new activities created

--- app/src/main/java/com/example/javainstagramclone/UploadActivity.java
+++ app/src/main/java/com/example/javainstagramclone/UploadActivity.java
@@ -1,7 +1,6 @@
 package com.example.javainstagramclone;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.javainstagramclone.databinding.ActivityMainBinding;
+public class UploadActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-
-    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-    }
-
-    public void signInClicked(View view){
-
-    }
-
-    public void signUpClicked(View view){
-
+        setContentView(R.layout.activity_upload);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
