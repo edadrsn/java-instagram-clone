7884a528904ab32afda9c9110d9e7a2be6665a02
Eda Dursun <edadrsn929@gmail.com>
2025-02-12 22:50:44 +0300

    methods for sign in and sign out buttons were created with firebase

--- app/src/main/java/com/example/javainstagramclone/FeedActivity.java
+++ app/src/main/java/com/example/javainstagramclone/FeedActivity.java
@@ -1,8 +1,13 @@
 package com.example.javainstagramclone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,17 +15,46 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.javainstagramclone.databinding.ActivityFeedBinding;
 import com.example.javainstagramclone.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FeedActivity extends AppCompatActivity {
 
     private ActivityFeedBinding binding;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         binding= ActivityFeedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.myToolbar);
+
+        auth=FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.options_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.add_post){
+            //Upload Activity
+            Intent intentToUpload=new Intent(FeedActivity.this,UploadActivity.class);
+            startActivity(intentToUpload);
+
+        }
+        else if(item.getItemId()==R.id.signout){
+            //Sign Out
+            auth.signOut();
+            Intent intentToMain=new Intent(FeedActivity.this,MainActivity.class);
+            startActivity(intentToMain);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
